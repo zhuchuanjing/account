add91c01d452f8fbddfb8d9e2838f14da1d00b27
zhuchuanjing <200721107@qq.com>
2024-09-18 18:27:01 +0800

    firt version

--- src/main.rs
+++ src/main.rs
@@ -14,18 +14,30 @@ use trade::{TRADES, get_trades, get_amount};
 use anyhow::Result;
 
 fn main()-> Result<()> {
+    fern::Dispatch::new()
+        .format(|out, message, record| {
+            out.finish(format_args!(
+                "{}[{}][{}] {} line: {}",
+                chrono::Local::now().format("[%Y-%m-%d][%H:%M:%S]"),
+                record.target(),
+                record.level(),
+                message,
+                record.line().unwrap_or_default()
+            ))
+        }).level(log::LevelFilter::Info).chain(fern::log_file("account.log")?).apply()?;
+    
     TRADES.load().unwrap();         //重启的时候加载所有的交易记录，注意 我们不处理没有终态的记录，这样可以保证数据的一致性，
 
     /*      从老的数据库迁移，只需要 读取所有已经完成的交易(成功的) 然后使用 add_trade 写入数据库就可以
-            
-            add_trade(Cow::from(key), trade);
-
+            for trade in [all success trade] {
+                add_trade(trade_id, trade_info);
+            }
      */
 
     let mut total = (0, 0);
     for i in 0..ADDRS.len() {
         get_trades(ADDRS[i].clone());
-        let amount = get_amount(ADDRS[i].clone()).unwrap();
+        let amount = get_amount(ADDRS[i].clone()).unwrap_or((0, 0));
         total.0 += amount.0;
         total.1 += amount.1;
     }
@@ -94,7 +106,7 @@ fn main()-> Result<()> {
             for i in 0..200 {
                 let tid = Cow::from(snowflaker::next_id_string().unwrap());
                 let from = Cow::from(ADDRS[i].clone());
-                let _ = trade::add_withdraw(tid.clone(), from, Cow::from(""), 100, 10, 10).map_err(|e| { println!("{:?}", e); });
+                let _ = trade::add_withdraw(tid.clone(), from, Cow::from(""), 100, 10, 10).map_err(|e| { log::error!("{:?}", e); });
                 tids.push(tid);
                 std::thread::sleep(std::time::Duration::from_millis(1));
             }
@@ -113,7 +125,7 @@ fn main()-> Result<()> {
         let mut total = (0, 0);
         for i in 0..ADDRS.len() {
             get_trades(ADDRS[i].clone());
-            let amount = get_amount(ADDRS[i].clone()).unwrap();
+            let amount = get_amount(ADDRS[i].clone()).unwrap_or((0, 0));
             total.0 += amount.0;
             total.1 += amount.1;
         }
